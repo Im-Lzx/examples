2b56d43cff4b8ae36bcf4a15b0883a72bb82097f
lizhaoxin <zhaoxin.li@uhomes.com>
2022-09-23 19:44:01 +0800

    补充iconfont

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="w-auto border-b py-1">
-    <div class="container mx-auto flex flex-row">
+  <div class="header">
+    <div class="container mx-auto flex flex-row justify-between">
       <div class="font-serif flex flex-row items-center">
         <img class="logo" src="@/assets/img/logo/mini.png" alt="logo" />
         <div>Demo</div>
       </div>
-      <div class="menu-group flex flex-row justify-start items-center">
-        <div class="menu">文档</div>
-        <div class="menu">关于</div>
-        <div class="menu">文档</div>
-        <div class="menu"></div>
+      <div class="menu-group flex flex-row justify-end items-center flex-1">
+        <a class="menu">文档</a>
+        <a class="menu">关于</a>
+        <a class="menu">赞助</a>
+        <a class="menu iconfont icon-language"></a>
+        <a class="menu iconfont icon-github-fill"></a>
+        <a class="menu iconfont icon-tuitetwitter44"></a>
       </div>
     </div>
   </div>
@@ -18,8 +20,18 @@
 <script setup></script>
 
 <style lang="scss" scoped>
+.header {
+  height: $header-h;
+  @apply flex items-center w-auto border-b py-1 box-border fixed inset-x-0 top-0;
+}
 .logo {
   width: 60px;
   height: 35px;
+}
+.menu {
+  @apply mr-6 text-base cursor-pointer hover:text-green-700;
+  &.iconfont {
+    @apply text-xl text-gray-500;
+  }
 }
 </style>
